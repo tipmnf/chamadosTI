86564e879ed6abce6a45f2e3b5dc4f916cda4360
PedroSchuenck <pschuenckbittencourt@gmail.com>
2023-10-24 16:00:47 -0300

    0.3.3 css in mainPage

--- chamadosApp/static/js/app.js
+++ chamadosApp/static/js/app.js
@@ -1,1 +1,13 @@
-console.log('oi');
+document.addEventListener('DOMContentLoaded', function() {
+    let buttonPesquisaChamado = document.getElementById("button-pesquisa-chamado");
+    let formMainPage = document.getElementById("form-pesquisa-chamado");
+
+    buttonPesquisaChamado.addEventListener('click', function(){
+        let computedStyle = window.getComputedStyle(formMainPage);
+        if (computedStyle.display === 'none') {
+            formMainPage.style.display = 'block';
+        } else {
+            formMainPage.style.display = 'none';
+        }
+    });
+});
